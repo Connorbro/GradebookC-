d6fe698ee2924af42641191d0690c3f6ccb62466
connor <vbrookesy@hotmail.co.uk>
2020-05-01 13:59:39 +0100

    Envoked a new event embedded into my if statement

--- Book.cs
+++ Book.cs
@@ -41,14 +41,17 @@ namespace GradeBook
             if(grade <= 100 && grade >=0)
             {
                 grades.Add(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
             }
             else 
             {
                 throw new ArgumentException($"Invalid {nameof(grade)}");
             }
-            
-
         }
+        public event GradeAddedDelegate GradeAdded;
         public Statistics GetStatistics()
         {
 
