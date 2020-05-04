518f2a3607638a923d485e9271bf562d1eb598b6
connor <vbrookesy@hotmail.co.uk>
2020-05-04 16:22:45 +0100

    Made a letter a property in statistics and cut pasted my switch statement inside there

--- Book.cs
+++ Book.cs
@@ -127,40 +127,7 @@ namespace GradeBook
                  
             }
 
-            switch(result.Average)
-            {
-                case var d when d >= 90.0:
-                result.Letter = 'A';
-                break;
-
-                case var d when d >= 80.0:
-                result.Letter = 'B';
-                break;
-
-                case var d when d >= 70.0:
-                result.Letter = 'C';
-                break;
-
-                case var d when d >= 60.0:
-                result.Letter = 'D';
-                break;
-
-                case var d when d >= 50.0:
-                result.Letter = 'E';
-                break;
-
-                case var d when d >= 40.0:
-                result.Letter = 'F';
-                break;
-
-                case var d when d >= 30.0:
-                result.Letter = 'G';
-                break;
-
-                case var d when d >= 20.0:
-                result.Letter = 'U';
-                break;
-            }
+            
 
             return result;
         }
--- Statistics.cs
+++ Statistics.cs
@@ -13,7 +13,43 @@ namespace GradeBook
         }
         public double High;
         public double Low;
-        public char Letter;
+        public char Letter
+        {
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                result.Letter = 'A';
+                break;
+
+                case var d when d >= 80.0:
+                result.Letter = 'B';
+                break;
+
+                case var d when d >= 70.0:
+                result.Letter = 'C';
+                break;
+
+                case var d when d >= 60.0:
+                result.Letter = 'D';
+                break;
+
+                case var d when d >= 50.0:
+                result.Letter = 'E';
+                break;
+
+                case var d when d >= 40.0:
+                result.Letter = 'F';
+                break;
+
+                case var d when d >= 30.0:
+                result.Letter = 'G';
+                break;
+
+                case var d when d >= 20.0:
+                result.Letter = 'U';
+                break;
+            }
+        }
         public double Sum;
         public int Count;
 
